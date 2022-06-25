11f19f4646e6e7098b15bd64429d79695a3f6736
Daniel Ortiz <ortiz.daniel1994@gmail.com>
2022-06-24 22:15:54 -0400

    Finished and moved CSS into a Public Folder. Set up a post request.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const { get } = require("request");
 
 const app = express();
+
+// Specifies a static folder where we have all of those files
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", function(req, res){
+    let firstName = req.body.fname;
+    let lastName = req.body.lname;
+    let email = req.body.email
+    console.log(firstName, lastName, email);
+})
 
 app.listen(3000, function(){
     console.log("Server is Working on Port 3000");
